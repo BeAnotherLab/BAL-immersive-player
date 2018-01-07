bc6199d80cb904758b3d7f7de59e8a2d462988d5
Marte Roel <marteroel@gmail.com>
2018-01-07 21:47:45 +0100

    Added file enlisting script

---
+++ Assets/Scripts/EnlistFiles.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class EnlistFiles : MonoBehaviour {
+
+	string path = "C:/Users/BeAnotherLab/Desktop/";
+
+	//DirectoryInfo dir = new DirectoryInfo(myPath)
+
+	// Use this for initialization
+	void Start () {
+		foreach (string file in System.IO.Directory.GetFiles(path)) {
+			Debug.Log (file);
+		}
+
+			
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
